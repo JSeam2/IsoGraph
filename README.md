fb46e7d5226fb3808229e468effdca4fd81e102d
JSeam <joshiaseam@gmail.com>
2017-01-13 16:41:43 +0800

    new updates

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # IsoGraph
-Generate the adjacency matrices of Isomorphic Graphs and Non-isomorphic graphs. 
+Generate the adjacency matrices of Isomorphic Graphs and Non-isomorphic graphs.
+The adjacency matrices are stored as numpy arrays for further use.
+
+The original intention for use was for an experiment which sought to simulate
+the implementation of a quantum machine learning algorithm on a classical/"normal"
+computer. The idea was to use some form of supervised learning technique to learn a 
+sequence of quantum gates to find a solution to a problem. The problem that was to be
+addressed was the Graph Isomorphism problem.
+
+Project status: Still working on it. 
--- gen_iso_graph.py
+++ gen_iso_graph.py
@@ -18,30 +18,63 @@ ______________________________________
 """
 
 import numpy as np
+import scipy as sp
+import networkx as nx
 
-# ignore anything below 3 nodes
-num_nodes = [x for x in range(3, 21)]
 
 # generate isomorphic graphs
-def gen_isograph(nodes):
+def gen_isograph(n):
     """
     Get no. of nodes as input
     Generate pairs of isomorphic graphs
     Verify the isomorphic graphs
-    Output the isomorphic graphs adjacency matrix
-    
+    IOutput the isomorphic graphs adjacency matrix
+
+    Some mathematical definition:
+    G ≅ H (G is isomorphic to H)
+
+    iff ∃ a: V(G)→ V(H)    (A bijection)
+    such that
+    a(u)a(v) ∈ E(H) ↔ uv ∈ E(G)
+
+    Similarly,
+    for some permutation matrix P,
+    G ≅ H  ↔ A_G = P* A_H *P_transpose
+
     param:
         nodes(int): number of node
-    
+
     output:
         numpy matrix
     """
-    
+    # Generate random graph, G1 and convert to numpy matrix
+    G1 = nx.dense_gnm_random_graph(n, n)
+    G1_numpy = to_numpy_matrix(G1)
+
+    # Generate random graph, G2 and convert to numpy matrix
+    G2 = nx.dense_gnm_random_graph(n, n)
+    G2_numpy = to_numpy_matrix(G2)
+
+    # Combine the matrices together
+    Gall = 
+
+
+    # after creating the graph need find a way to find an isomorphic graph to
+    # this randomly generating graphs won't allow you to find isomorphic pairs
+
     return graph
+
 
 def save_graph(graph):
     """
     save the graphs in to a npz file
     label it respectively and place it into a ./isomorphic graphs folder
+    """
+    pass
 
 
+if __name__ == "__main__":
+
+    
+    # ignore anything below 3 nodes
+    # num_nodes = [x for x in range(3, 21)]
